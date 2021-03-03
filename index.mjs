89995ad0145c27cf92cffd3a4778a71b9ad8f3f4
Bryant <bryant@keystrokecode.com>
2021-03-02 17:17:51 -0800

    expose other packages to notebook

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
 import Observable from '@observablehq/runtime';
 import notebook from '@keystroke/observablehq';
 import express from 'express';
+import cors from 'cors';
+import puppeteer from 'puppeteer';
 
-runNotebook(notebook, { process, express });
+runNotebook(notebook, { process, express, cors, puppeteer });
 
 async function runNotebook(notebook, context = { process }, cell = 'main') {
     return (await new Observable.Runtime().module(notebook).value(cell))(context);
