ecba48273ac83a461d5e37a71640eb46f53572d3
Bryant <bryant@keystrokecode.com>
2021-05-01 17:37:36 -0700

    Ingore package-lock.json

--- index.mjs
+++ index.mjs
@@ -4,8 +4,9 @@ import express from 'express';
 import cors from 'cors';
 import puppeteer from 'puppeteer';
 
-runNotebook(notebook, { process, express, cors, puppeteer });
+runNotebook(notebook, { process, express, cors, puppeteer })
+    .catch(console.error.bind(console));
 
 async function runNotebook(notebook, context = { process }, cell = 'main') {
-    return (await new Observable.Runtime().module(notebook).value(cell))(context);
+    return await (await new Observable.Runtime().module(notebook).value(cell))(context);
 }
