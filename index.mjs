047f3257ffa2fd6418d50dcd740ae25dfe545a7d
Bryant <bryant@keystrokecode.com>
2021-05-10 19:44:19 -0700

    add node fetch and etag support

--- index.mjs
+++ index.mjs
@@ -3,8 +3,9 @@ import notebook from '@keystroke/observablehq';
 import express from 'express';
 import cors from 'cors';
 import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
 
-runNotebook(notebook, { process, express, cors, puppeteer })
+runNotebook(notebook, { process, express, cors, puppeteer, fetch })
     .catch(console.error.bind(console));
 
 async function runNotebook(notebook, context = { process }, cell = 'main') {
